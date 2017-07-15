d10a9a3375103975a5cbcd0248a62a1b0d33b7d7
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2017-07-15 22:14:49 +0300

    Added a when expression

--- src/main/kotlin/il/co/kotlintlv/solid/basics2/Person.kt
+++ src/main/kotlin/il/co/kotlintlv/solid/basics2/Person.kt
@@ -6,8 +6,21 @@ sealed class Person(val firstName: String,
     fun fullName() = "$firstName $lastName"
 
     class Dor: Person("Dor", "Samet")
-    class Alex(val age: Int, fName: String, lastName: String) : Person(fName, lastName)
+    class Alex(val age: Int) : Person("Alex", "Gherschon")
     /*data class Alex(val age: Int,
                     override val firstName:
                     String, override val lastName: String) : Person(lastName, firstName)*/
 }
+
+fun translatePerson(person: Person): String =
+        when (person) {
+            is Person.Dor -> person.fullName()
+            is Person.Alex -> "${person.firstName} ${person.lastName}, of age ${person.age}"
+        }
+
+fun main(args: Array<String>) {
+
+    println(translatePerson(Person.Dor()))
+    println(translatePerson(Person.Alex(32)))
+}
+
